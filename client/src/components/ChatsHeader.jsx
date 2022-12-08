1dbdd6435a1efb20758469260fd998119f90aea3
Otulugbu Alexander <s2200742@Alexander-MacBook-Air.local>
2022-12-08 16:51:59 +0200

    Commits: Implemented Dashboard Container

--- client/src/components/ChatsHeader.jsx
+++ client/src/components/ChatsHeader.jsx
@@ -5,9 +5,9 @@ const ChatsHeader = () => {
         <div className='chat-container-header'>
             <div className="profile">
             <div className="img-container">
-                <img src="" alt="" />
+             <img src="https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="" />
             </div>
-            <h2>UserNames</h2>
+            <h3>UserNames</h3>
             </div>      
             <i className="log-out-icon">x</i>              
         </div>
