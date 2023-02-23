b1ba16fa379c4115748dfd4edf43b6f46b0384c6
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2023-02-23 17:57:24 +0200

    fixed conflicts

--- client/src/components/AuthModal1.jsx
+++ client/src/components/AuthModal1.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { sendSignInLinkToEmail } from "firebase/auth";
+// import { sendSignInLinkToEmail } from "firebase/auth";
 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
